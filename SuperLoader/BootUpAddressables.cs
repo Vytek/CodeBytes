d25caa92bb80d5f3de5b956a1fde3faa9b78e41f
Badgerdox <50283631+Badgerdox@users.noreply.github.com>
2019-08-28 18:39:47 -0700

    Create BootUpAddressables.cs

---
+++ SuperLoader/BootUpAddressables.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public static class AddressableLoader
+{
+    public static IEnumerator Initialize<T>(string assetLabel, List<T> assets)
+    {
+        yield return Addressables.LoadAssetsAsync<T>(assetLabel, op =>
+        {
+            assets.Add(op);
+        });
+    }
+    
+    public static IEnumerator InitCreateDeactivate<T>(string assetLabel, List<T> assets) where T : Object
+    {
+        yield return  Addressables.LoadAssetsAsync<T>(assetLabel, op =>
+        {
+            assets.Add(GameObject.Instantiate(op));
+        });
+
+        foreach (var asset in assets)
+        {
+            GameObject.Find(asset.name).gameObject.SetActive(false);
+        }
+    }
+}
